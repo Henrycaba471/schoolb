778eedc1e9636e6969365b1d9af5b12a0308b69e
Henry Cañizares <henrycaba471@gmail.com>
2025-10-08 10:34:39 -0500

    Se actualizo port en backend

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ app.use('/api/temas', temaRoutes);
 
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 11000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
